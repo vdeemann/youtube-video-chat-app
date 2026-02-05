dc5271d6356f2d08417503af03db7158ef01d1df
Dee Mann <vdmann@protonmail.com>
2026-02-05 11:15:26 -0700

    Disable Swoosh API client in production to fix hackney dependency error

--- config/prod.exs
+++ config/prod.exs
@@ -3,4 +3,8 @@ import Config
 # Do not print debug messages in production
 config :logger, level: :info
 
+# Disable Swoosh API client in production (we're not sending emails)
+# This avoids the hackney dependency requirement
+config :swoosh, :api_client, false
+
 # Runtime production config is in config/runtime.exs
